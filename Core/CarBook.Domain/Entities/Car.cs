5679f4062016c1cc6afaf5baa0f5dfccd4d9fe19
Özge Kestane <ozgekestane97@gmail.com>
2024-07-25 20:41:31 +0300

    Car & CarPricing & Contact & Pricing entities added

--- Core/CarBook.Domain/Entities/Car.cs
+++ Core/CarBook.Domain/Entities/Car.cs
@@ -15,6 +15,7 @@
         public string BigImageUrl { get; set; }
         public List<CarFeature> CarFeatures { get; set; }
         public List<CarDescription> CarDescriptions { get; set; }
+        public List<CarPricing> CarPricings { get; set; }
 
     }
 }
