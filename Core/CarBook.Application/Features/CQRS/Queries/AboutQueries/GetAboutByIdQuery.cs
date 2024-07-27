e66d02c55ff87633533ecee71b470cde420df108
Özge Kestane <ozgekestane97@gmail.com>
2024-07-28 02:26:43 +0300

    CQRS folder created, About(Handlers, Queries, Results) added

---
+++ Core/CarBook.Application/Features/CQRS/Queries/AboutQueries/GetAboutByIdQuery.cs
@@ -0,0 +1,13 @@
+﻿namespace CarBook.Application.Features.CQRS.Queries.AboutQueries
+{
+    public class GetAboutByIdQuery
+    {
+        public GetAboutByIdQuery(int id)
+        {
+            Id = id;
+        }
+
+        public int Id { get; set; }
+
+    }
+}
