51372e1570b16a200558b0e738ef6f6f6047393f
Özge Kestane <ozgekestane97@gmail.com>
2024-07-31 21:27:50 +0300

    Banner entities CQRS op. added

---
+++ Core/CarBook.Application/Features/CQRS/Commands/BannerCommands/RemoveBannerCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace CarBook.Application.Features.CQRS.Commands.BannerCommands
+{
+    public class RemoveBannerCommand
+    {
+        public RemoveBannerCommand(int id)
+        {
+            Id = id;
+        }
+
+        public int Id { get; set; }
+
+    }
+}
