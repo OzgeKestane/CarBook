d70c3d0eb16e0c530833de1d1cd77e9f62de11e3
Özge Kestane <ozgekestane97@gmail.com>
2024-08-02 01:03:57 +0300

    Brand entity CQRS op. added

---
+++ Core/CarBook.Application/Features/CQRS/Handlers/BrandHandlers/GetBrandQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using CarBook.Application.Features.CQRS.Results.BrandResults;
+using CarBook.Application.Interfaces;
+using CarBook.Domain.Entities;
+
+namespace CarBook.Application.Features.CQRS.Handlers.BrandHandlers
+{
+    public class GetBrandQueryHandler
+    {
+        private readonly IRepository<Brand> _brandRepository;
+
+        public GetBrandQueryHandler(IRepository<Brand> brandRepository)
+        {
+            _brandRepository = brandRepository;
+        }
+        public async Task<List<GetBrandQueryResult>> Handle()
+        {
+            var values = await _brandRepository.GetAllAsync();
+            return values.Select(x => new GetBrandQueryResult
+            {
+                BrandId = x.BrandId,
+                Name = x.Name
+            }).ToList();
+        }
+    }
+}
---
+++ Core/CarBook.Application/Features/CQRS/Handlers/BrandHandlers/UpdateBrandCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using CarBook.Application.Features.CQRS.Commands.BrandCommands;
+using CarBook.Application.Interfaces;
+using CarBook.Domain.Entities;
+
+namespace CarBook.Application.Features.CQRS.Handlers.BrandHandlers
+{
+    public class UpdateBrandCommandHandler
+    {
+        private readonly IRepository<Brand> _brandRepository;
+
+        public UpdateBrandCommandHandler(IRepository<Brand> brandRepository)
+        {
+            _brandRepository = brandRepository;
+        }
+        public async Task Handle(UpdateBrandCommand command)
+        {
+            var values = await _brandRepository.GetByIdAsync(command.BrandId);
+            values.Name = command.Name;
+            await _brandRepository.UpdateAsync(values);
+        }
+    }
+}
