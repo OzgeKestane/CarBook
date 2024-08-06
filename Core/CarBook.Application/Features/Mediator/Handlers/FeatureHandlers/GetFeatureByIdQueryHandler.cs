e7651e2e9ce58ba0c09f746493cd1a1cfa9c151a
Özge Kestane <ozgekestane97@gmail.com>
2024-08-06 14:42:52 +0300

    Feature Command & Handler added

---
+++ Core/CarBook.Application/Features/Mediator/Handlers/FeatureHandlers/GetFeatureByIdQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using CarBook.Application.Features.Mediator.Queries.FeatureQueries;
+using CarBook.Application.Features.Mediator.Results.FeatureResults;
+using CarBook.Application.Interfaces;
+using CarBook.Domain.Entities;
+using MediatR;
+
+namespace CarBook.Application.Features.Mediator.Handlers.FeatureHandlers
+{
+    public class GetFeatureByIdQueryHandler : IRequestHandler<GetFeatureByIdQuery, GetFeatureByIdQueryResult>
+    {
+        private readonly IRepository<Feature> _featureRepository;
+
+        public GetFeatureByIdQueryHandler(IRepository<Feature> featureRepository)
+        {
+            _featureRepository = featureRepository;
+        }
+
+        public async Task<GetFeatureByIdQueryResult> Handle(GetFeatureByIdQuery request, CancellationToken cancellationToken)
+        {
+            var values = await _featureRepository.GetByIdAsync(request.Id);
+            return new GetFeatureByIdQueryResult
+            {
+                FeatureId = request.Id,
+                Name = values.Name
+            };
+        }
+    }
+}
