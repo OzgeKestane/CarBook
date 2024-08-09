15ec3a936e651d702b9491f4d9733ff0b1c97239
Özge Kestane <ozgekestane97@gmail.com>
2024-08-10 01:19:21 +0300

    SocialMedia Handlers added

--- Core/CarBook.Application/Features/Mediator/Queries/SocialMediaQueries/GetSocialMediaByIdQuery.cs
+++ Core/CarBook.Application/Features/Mediator/Queries/SocialMediaQueries/GetSocialMediaByIdQuery.cs
@@ -1,9 +1,9 @@
-﻿using CarBook.Application.Features.Mediator.Results.ServiceResults;
+﻿using CarBook.Application.Features.Mediator.Results.SocialMediaResults;
 using MediatR;
 
 namespace CarBook.Application.Features.Mediator.Queries.SocialMediaQueries
 {
-    public class GetSocialMediaByIdQuery : IRequest<GetServiceByIdQueryResult>
+    public class GetSocialMediaByIdQuery : IRequest<GetSocialMediaByIdQueryResult>
     {
         public int Id { get; set; }
 
