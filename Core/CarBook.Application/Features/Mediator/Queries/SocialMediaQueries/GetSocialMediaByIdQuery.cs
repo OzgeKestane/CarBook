a32bbb62de44a81d14356dd4c9e24772b23abecf
Özge Kestane <ozgekestane97@gmail.com>
2024-08-09 19:57:27 +0300

    SocialMedia entity Queries & Results added

---
+++ Core/CarBook.Application/Features/Mediator/Queries/SocialMediaQueries/GetSocialMediaByIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using CarBook.Application.Features.Mediator.Results.ServiceResults;
+using MediatR;
+
+namespace CarBook.Application.Features.Mediator.Queries.SocialMediaQueries
+{
+    public class GetSocialMediaByIdQuery : IRequest<GetServiceByIdQueryResult>
+    {
+        public int Id { get; set; }
+
+        public GetSocialMediaByIdQuery(int id)
+        {
+            Id = id;
+        }
+    }
+}
