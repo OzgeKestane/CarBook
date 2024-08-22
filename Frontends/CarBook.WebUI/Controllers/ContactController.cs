ea94610a1d525bb82cf050d58f27646a2876de9b
Özge Kestane <ozgekestane97@gmail.com>
2024-08-22 14:53:57 +0300

    Message Send Op.

--- Frontends/CarBook.WebUI/Controllers/ContactController.cs
+++ Frontends/CarBook.WebUI/Controllers/ContactController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook.Dto.ContactDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace CarBook.WebUI.Controllers
 {
@@ -10,9 +13,23 @@ namespace CarBook.WebUI.Controllers
         {
             _httpClientFactory = httpClientFactory;
         }
-
+        [HttpGet]
         public IActionResult Index()
         {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(CreateContactDto createContactDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            createContactDto.SendDate = DateTime.Now;
+            var jsonData = JsonConvert.SerializeObject(createContactDto);
+            StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7227/api/Contacts", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Default");
+            }
             return View();
         }
     }
