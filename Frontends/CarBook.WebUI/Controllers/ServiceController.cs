7fdb727b924991d63ba9054c1f11b0969592cd81
Özge Kestane <ozgekestane97@gmail.com>
2024-08-23 19:16:17 +0300

    Services added HompePage

--- Frontends/CarBook.WebUI/Controllers/ServiceController.cs
+++ Frontends/CarBook.WebUI/Controllers/ServiceController.cs
@@ -1,28 +1,13 @@
-﻿using CarBook.Dto.ServiceDtos;
-using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace CarBook.WebUI.Controllers
 {
     public class ServiceController : Controller
     {
-        private readonly IHttpClientFactory _httpClientFactory;
 
-        public ServiceController(IHttpClientFactory httpClientFactory)
-        {
-            _httpClientFactory = httpClientFactory;
-        }
 
-        public async Task<IActionResult> Index()
+        public IActionResult Index()
         {
-            var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("https://localhost:7227/api/Services");
-            if (responseMessage.IsSuccessStatusCode)
-            {
-                var jsonData = await responseMessage.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<List<ResultServiceDto>>(jsonData);
-                return View(values);
-            }
             return View();
         }
     }
