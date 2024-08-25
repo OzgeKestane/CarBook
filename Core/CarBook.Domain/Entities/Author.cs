26d33f16f53f7db4068a98b07e43e05799a1cb05
Özge Kestane <ozgekestane97@gmail.com>
2024-08-25 17:44:48 +0300

    Blog & Author entities created

---
+++ Core/CarBook.Domain/Entities/Author.cs
@@ -0,0 +1,13 @@
+﻿namespace CarBook.Domain.Entities
+{
+    public class Author
+    {
+        public int AuthorId { get; set; }
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        public string Description { get; set; }
+        public List<Blog> Blogs { get; set; }
+
+
+    }
+}
