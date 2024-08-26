96ee87b724c7e13f686f72c5bd0b0cc3ccfc8d48
Özge Kestane <ozgekestane97@gmail.com>
2024-08-26 15:09:24 +0300

    Author Queries added

---
+++ Core/CarBook.Application/Features/Mediator/Queries/AuthorQueries/GetAuthorByIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using CarBook.Application.Features.CQRS.Results.AboutResults;
+using MediatR;
+
+namespace CarBook.Application.Features.Mediator.Queries.AuthorQueries
+{
+    public class GetAuthorByIdQuery : IRequest<GetAboutByIdQueryResult>
+    {
+        public int Id { get; set; }
+
+        public GetAuthorByIdQuery(int id)
+        {
+            Id = id;
+        }
+    }
+}
