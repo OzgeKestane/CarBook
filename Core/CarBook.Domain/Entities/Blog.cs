a3632541cef94319cbc8003bef9b366d46131197
Özge Kestane <ozgekestane97@gmail.com>
2024-08-28 03:56:57 +0300

    Blog entity prop. fixed

--- Core/CarBook.Domain/Entities/Blog.cs
+++ Core/CarBook.Domain/Entities/Blog.cs
@@ -4,7 +4,7 @@
     {
         public int BlogId { get; set; }
         public string Title { get; set; }
-        public string AuthorId { get; set; }
+        public int AuthorId { get; set; }
         public Author Author { get; set; }
         public string CoverImageUrl { get; set; }
         public DateTime CreatedDate { get; set; }
