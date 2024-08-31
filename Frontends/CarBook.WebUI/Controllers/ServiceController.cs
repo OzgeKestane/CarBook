a7fecf6f6edd648e260832b7108187c56cc8f231
Özge Kestane <ozgekestane97@gmail.com>
2024-08-31 16:09:05 +0300

    Car & Service Controller updated

--- Frontends/CarBook.WebUI/Controllers/ServiceController.cs
+++ Frontends/CarBook.WebUI/Controllers/ServiceController.cs
@@ -4,10 +4,11 @@ namespace CarBook.WebUI.Controllers
 {
     public class ServiceController : Controller
     {
-
-
         public IActionResult Index()
         {
+            ViewBag.v1 = "Hizmetler";
+            ViewBag.v2 = "Hizmetlerimiz";
+
             return View();
         }
     }
