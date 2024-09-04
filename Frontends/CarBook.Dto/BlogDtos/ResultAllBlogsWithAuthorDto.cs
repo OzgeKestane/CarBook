5f3767cd77c50c1df4af284a364a3f1c6941421b
Özge Kestane <ozgekestane97@gmail.com>
2024-09-04 23:28:39 +0300

    AllBlogsWithAuthor UI consume

---
+++ Frontends/CarBook.Dto/BlogDtos/ResultAllBlogsWithAuthorDto.cs
@@ -0,0 +1,17 @@
+﻿namespace CarBook.Dto.BlogDtos
+{
+    public class ResultAllBlogsWithAuthorDto
+    {
+
+        public int blogId { get; set; }
+        public string title { get; set; }
+        public string authorName { get; set; }
+        public string categoryName { get; set; }
+        public int authorId { get; set; }
+        public string coverImageUrl { get; set; }
+        public DateTime createdDate { get; set; }
+        public int categoryId { get; set; }
+
+
+    }
+}
