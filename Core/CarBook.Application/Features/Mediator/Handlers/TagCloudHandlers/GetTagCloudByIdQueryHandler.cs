8c8fedfcad631031979cfd6341b2d87866b119b7
Özge Kestane <ozgekestane97@gmail.com>
2024-09-10 18:57:44 +0300

    TagClouf for mediator handlers added

---
+++ Core/CarBook.Application/Features/Mediator/Handlers/TagCloudHandlers/GetTagCloudByIdQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using CarBook.Application.Features.Mediator.Queries.TagCloudQueries;
+using CarBook.Application.Features.Mediator.Results.TagCloudResults;
+using CarBook.Application.Interfaces;
+using CarBook.Domain.Entities;
+using MediatR;
+
+namespace CarBook.Application.Features.Mediator.Handlers.TagCloudHandlers
+{
+    public class GetTagCloudByIdQueryHandler : IRequestHandler<GetTagCloudByIdQuery, GetTagClougByIdQueryResult>
+    {
+        private readonly IRepository<TagCloud> _repository;
+
+        public GetTagCloudByIdQueryHandler(IRepository<TagCloud> repository)
+        {
+            _repository = repository;
+        }
+        public async Task<GetTagClougByIdQueryResult> Handle(GetTagCloudByIdQuery request, CancellationToken cancellationToken)
+        {
+            var values = await _repository.GetByIdAsync(request.Id);
+            return new GetTagClougByIdQueryResult
+            {
+                BlogId = values.BlogId,
+                TagCloudId = values.TagCloudId,
+                Title = values.Title
+            };
+        }
+    }
+}
