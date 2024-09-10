3aa9634e3695b3be4678f858447eeb0294623ee9
Özge Kestane <ozgekestane97@gmail.com>
2024-09-10 18:12:53 +0300

    TagCloud Entity added

--- Core/CarBook.Domain/Entities/Blog.cs
+++ Core/CarBook.Domain/Entities/Blog.cs
@@ -11,5 +11,6 @@
         public int CategoryId { get; set; }
         public Category Category { get; set; }
         public string Description { get; set; }
+        public List<TagCloud> TagClouds { get; set; }
     }
 }
