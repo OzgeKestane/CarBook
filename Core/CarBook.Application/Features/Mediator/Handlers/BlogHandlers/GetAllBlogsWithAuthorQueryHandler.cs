19e60f66abdebd2d985c95d54ab1156ecf931442
Özge Kestane <ozgekestane97@gmail.com>
2024-09-17 01:19:35 +0300

    updated get all blogs with author

--- Core/CarBook.Application/Features/Mediator/Handlers/BlogHandlers/GetAllBlogsWithAuthorQueryHandler.cs
+++ Core/CarBook.Application/Features/Mediator/Handlers/BlogHandlers/GetAllBlogsWithAuthorQueryHandler.cs
@@ -27,7 +27,9 @@ namespace CarBook.Application.Features.Mediator.Handlers.BlogHandlers
                 CreatedDate = x.CreatedDate,
                 AuthorName = x.Author.Name,
                 CategoryName = x.Category.Name,
-                Description = x.Description
+                Description = x.Description,
+                AuthorDescription = x.Author.Description,
+                AuthorImageUrl = x.Author.ImageUrl
             }).ToList();
         }
     }
