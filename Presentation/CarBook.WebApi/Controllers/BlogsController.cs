60840035b96a21036ddf8b33f9395625582fef39
Özge Kestane <ozgekestane97@gmail.com>
2024-09-18 15:56:58 +0300

    fix

--- Presentation/CarBook.WebApi/Controllers/BlogsController.cs
+++ Presentation/CarBook.WebApi/Controllers/BlogsController.cs
@@ -58,7 +58,7 @@ namespace CarBook.WebApi.Controllers
             return Ok(values);
         }
         [HttpGet("GetBlogByAuthorIdList")]
-        public async Task<IActionResult> GetBlogByAuthorIdList(int id)
+        public async Task<IActionResult> GetBlogByAuthorId(int id)
         {
             var values = await _mediator.Send(new GetBlogByAuthorIdQuery(id));
             return Ok(values);
